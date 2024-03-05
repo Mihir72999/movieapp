f372c3df3e3d5626a4613306a38d4261eb18b66f
mihir72999 <mihir72999@gmail.com>
2024-03-05 15:30:18 +0530

    first commit

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -7,8 +7,8 @@ const MovieCard = ({movieData }) => {
      <div  className='grid grid-col-2 w-[31vw] lg:w-[16vw]  '>
        <img className='h-[16vh]  xl:h-[50vh]  lg:h-[30vh] w-[31vw] lg:w-[16vw]  rounded-t-xl' src={movieData.Poster} alt={movieData.Title}/>
        <div className='flex bg-gray-500 lg:bg-black text-white px-3 py-2 rounded-b-xl'>
-          {movieData.imdbRating === "N/A" && <span className='flex items-center gap-2 text-sm lg:text-2xl'><ThumbsUp className='lg:w-6 w-4 text-green-500' />{Math.floor(Math.random() * 100)}k likes </span>}
-          {movieData.imdbRating !== "N/A" && <span className='flex items-center gap-2 text-sm lg:text-2xl'><Star className='w-4 lg:w-6  text-pink-700' /> {movieData.imdbRating}/10 rating</span>}
+          {movieData.imdbRating === "N/A" && <span className='flex items-center gap-2 text-xs lg:text-2xl'><ThumbsUp className='lg:w-6 w-4 text-green-500' />{Math.floor(Math.random() * 100)}k likes </span>}
+          {movieData.imdbRating !== "N/A" && <span className='flex items-center gap-2 text-xs lg:text-2xl'><Star className='w-4 lg:w-6  text-pink-700' /> {movieData.imdbRating}/10 rating</span>}
           </div>
        </div>
        <div className=' max-w-32 lg:max-w-60 py-2 font-medium'>
