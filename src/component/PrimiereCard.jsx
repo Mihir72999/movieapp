fbbbd318270b63ca38588b97779efda72b9dc66f
mihir72999 <mihir72999@gmail.com>
2024-03-05 15:23:29 +0530

    first commit

--- src/component/PrimiereCard.jsx
+++ src/component/PrimiereCard.jsx
@@ -4,8 +4,8 @@ const PrimiereCard = ({primierData}) => {
   return (
     <>
     <div className='text-white  flex flex-col ' >
-    <div className='grid grid-col-1 w-[31vw] xl:w-[18vw]   '>
-     <img  className=' h-[20vh] xl:h-[50vh] px-1 lg:px-0 w-[31vw] xl:w-[16vw] rounded-xl' src={primierData.Poster} alt={primierData.Title}/>
+    <div className='grid grid-col-1 w-[31vw] lg:w-[18w] xl:w-[18vw]   '>
+     <img  className=' h-[20vh] lg:h-[50vh] xl:h-[50vh] px-1 lg:px-0 w-[31vw] lg:w-[16vw] xl:w-[16vw] rounded-xl' src={primierData.Poster} alt={primierData.Title}/>
     <h3 className='lg:px-0 px-2 py-2'>{primierData.Title}</h3>
     <p className='lg:px-0 px-2 '>{primierData.Language}</p>
     </div>
